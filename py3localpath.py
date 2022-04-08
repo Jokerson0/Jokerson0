7e97cf7da5190fff32640610a9010437ae1b5f07
Jokerson0 <103249379+Jokerson0@users.noreply.github.com>
2022-04-08 17:23:32 +0800

    Update py3localpath.py

--- py3localpath.py
+++ py3localpath.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # Modified file from 
 # https://github.com/jafingerhut/p4-guide/blob/master/bin/py3localpath.py
-
 import re
 import sys
-
 l1=[x for x in sys.path if re.match(r'/usr/local/lib/python3.[0-9]+/dist-packages$', x)]
-
 if len(l1) == 1:
     m = re.match(r'(/usr/local/lib/python3.[0-9]+)/dist-packages$', l1[0])
     if m:
@@ -18,5 +15,4 @@ else:
     print("Found %d matching entries in Python3 sys.path instead of 1: %s."
           % (len(l1), l1))
     sys.exit(1)
-
 sys.exit(0)
